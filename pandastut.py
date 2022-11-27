cb3410dce37e4abb43a4bcfef118bbca5f8abe7c
raajendra9 <rajendrareddy.akkala@gmail.com>
2022-11-27 18:23:32 +0530

    pandas

--- pandastut.py
+++ pandastut.py
@@ -393,3 +393,17 @@ df.drop_duplicates(inplace = True)
 
 
 
+# Data Correlations
+# Finding relationships
+# corr() method  calulates the relationship between each column in your data set
+# The examples in this page uses csv file called 'data.csv'
+df.corr()
+# The corr() method ignores 'not numeric' columns
+
+# Result Explained 
+# The Result of the corr() method is a table with a lot of numbers that represents how well the relationship is between two columns.
+# The numbers varies from -1 to 1.
+# 1 means that there is a 1 to 1 relationship, and for this data set, each time a value went up in the first column, the other one went up as well
+# 0.9  is also a good ralationship, and if you increase one value, the other will probably go down
+# -0.9 would be just as good relationship as 0.9, but as you increase one value, the other will probably go down
+# 0.2 means not a good relationship, meaning that if one value goes up does not mean that the other will. 
